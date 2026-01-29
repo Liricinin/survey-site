2843494edd4931560881230492132cb373ae5773
Liricinin <fanshu090909@gmail.com>
2026-01-29 22:08:12 +0300

    Update app.py

--- survey_site/app.py
+++ survey_site/app.py
@@ -1,19 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        return "СПАСИБО, ОПРОС ПРОЙДЕН"
-    return """
-    <form method="post">
-      <p>Какой жанр нравится?</p>
-      <input type="radio" name="genre" value="Древнерусская"> Древнерусская<br>
-      <input type="radio" name="genre" value="Современная"> Современная<br>
-      <input type="submit">
-    </form>
-    """
+@app.route("/")
+def home():
+    return "Сайт работает ✅"
 
 if name == "__main__":
     app.run()
