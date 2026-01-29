06df478a3e0085f6d9e6073ba70b219b82d6ce8b
Liricinin <fanshu090909@gmail.com>
2026-01-29 22:17:30 +0300

    Update app.py

--- survey_site/app.py
+++ survey_site/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Сайт работает ✅"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        answer = request.form.get("genre")
+        return f"Ты выбрал: {answer}"
+    return render_template("survey.html")
 
 if __name__ == "__main__":
     app.run()
