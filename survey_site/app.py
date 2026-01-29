960c3858df3ec999c50f1583552efea245df6034
Liricinin <fanshu090909@gmail.com>
2026-01-29 23:22:55 +0300

    Update app.py

--- survey_site/app.py
+++ survey_site/app.py
@@ -3,9 +3,10 @@ import requests
 
 app = Flask(__name__)
 
-# 🔴 ВСТАВЬ СЮДА
+
 BOT_TOKEN = "8558607119:AAFOGlDrITQ3IFMcYiDVXeQnNp_Y_jgwz5c"
-CHAT_ID = "8558607119:AAFOGlDrITQ3IFMcYiDVXeQnNp_Y_jgwz5c"
+CHAT_ID = "Т8558607119"
+
 
 def send_to_telegram(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -15,13 +16,18 @@ def send_to_telegram(text):
     }
     requests.post(url, data=data)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        answer = request.form.get("genre")
+ 
+    if request.cookies.get("survey_done"):
+        return redirect("/done")
 
-        # ✈️ отправка в Telegram
-        send_to_telegram(f"📩 Новый ответ:\n{answer}")
+    if request.method == "POST":
+        answer = request.form.get("answer")
+
+        if answer:
+            send_to_telegram(f"📊 Новый ответ:\n{answer}")
 
         response = make_response(redirect("/done"))
         response.set_cookie("survey_done", "yes", max_age=60*60*24*365)
@@ -29,9 +35,11 @@ def index():
 
     return render_template("survey.html")
 
+
 @app.route("/done")
 def done():
     return render_template("done.html")
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
