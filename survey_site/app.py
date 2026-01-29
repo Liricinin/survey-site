bf60e5185359e070111ed7f9486fccb2175e30fd
Liricinin <fanshu090909@gmail.com>
2026-01-29 22:35:46 +0300

    Update app.py

--- survey_site/app.py
+++ survey_site/app.py
@@ -1,13 +1,4 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        answer = request.form.get("genre")
-        return f"Ты выбрал: {answer}"
-    return render_template("survey.html")
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/done")
+def done():
+    choice = request.args.get("genre")
+    return render_template("done.html", choice=choice)
