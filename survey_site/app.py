362951cf3e61afd5ae3242812c1eacc0e29ccbd7
Liricinin <fanshu090909@gmail.com>
2026-01-29 22:55:02 +0300

    Update app.py

--- survey_site/app.py
+++ survey_site/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,13 +6,21 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         genre = request.form.get("genre")
-        return redirect(url_for("done", genre=genre))
+
+        # сохраняем ответ
+        with open("answers.txt", "a", encoding="utf-8") as f:
+            f.write(genre + "\n")
+
+        return redirect("/done?genre=" + genre)
+
     return render_template("survey.html")
+
 
 @app.route("/done")
 def done():
-    genre = request.args.get("genre", "—")
+    genre = request.args.get("genre")
     return render_template("done.html", genre=genre)
 
-if __name__ == "__main__":
+
+if name == "__main__":
     app.run()
