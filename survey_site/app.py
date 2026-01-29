f0a0fb09c25b69f4032d549ec8592acb258a82a8
Liricinin <fanshu090909@gmail.com>
2026-01-29 22:40:56 +0300

    Update app.py

--- survey_site/app.py
+++ survey_site/app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, request, redirect, make_response
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        genre = request.form.get("genre")
+        return redirect(f"/done?genre={genre}")
+    return render_template("survey.html")
+
 @app.route("/done")
 def done():
     choice = request.args.get("genre")
